61e4ea8fb9321ec450778255b36e4752df91ac5a
Shrekich69 <leniviesosochki@yandex.ru>
2026-06-12 22:02:17 +0500

    Class Grouper now can actually group

--- scripts/grouper.js
+++ scripts/grouper.js
@@ -1,12 +1,14 @@
 "use strict"
 
+/**
+ * @interface
+ */
 class Group {
    collapsed = false;
-   color = "";
-   id = 0;
+   color = "grey";
+   id = -1;
    title = "";
 
-   excluded = false;
    extendable = true;
    domain = "";
    // amount of tabs
@@ -15,20 +17,124 @@ class Group {
 
 class Grouper {
 
-   static groups = new Array(Group);
+   grouping = true;
+   groups = new Array(Group);
 
    /**
-    * @returns {Object[]}
+    * Adds already existing groups into the array
     */
-   get groups() {
-      return this.groups;
+   async constructor() {
+      const exGroups = await browser.tabGroups.query({});
+      if (exGroups.length !== 0)
+      {
+         for (let i = 0; i < exGroups.length; i++)
+         {
+            let newGroup = new Group;
+
+            newGroup.collapsed = exGroups[i].collapsed;
+            newGroup.color = exGroups[i].color;
+            newGroup.id = exGroups[i].id;
+            newGroup.title = exGroups[i].title;
+
+            const tabsInGroup = await browser.tabs.query({ groupId: exGroups[i].id });
+            newGroup.domain = ParseUrl(tabsInGroup[0].url);
+            newGroup.amount = tabsInGroup.length;
+
+            this.groups.push(newGroup);
+         }
+      }
+   }
+
+   /**
+    * @param {string} url url of a tab
+    * @returns {string} domain of the passed url
+    */
+   static ParseUrl(url) {
+      if (!url || typeof url !== 'string') {
+         return '';
+      }
+
+      // Proceed special urls
+      if (url.startsWith("about:") || url.startsWith("file:") || url.startsWith("chrome:") || 
+         url.startsWith("moz-extension:") || url.startsWith("chrome-extension:")) {
+         return url;
+      }
+
+      // Cut the beginning of the url
+      if (url.startsWith("https://"))
+         url = url.substring(8);
+      else if (url.startsWith("http://"))
+         url = url.substring(7);
+
+      const slashIndex = url.indexOf('/');
+      if (slashIndex !== -1) {
+         url = url.substring(0, slashIndex); 
+      }
+      
+      return url;
    }
 
    /**
     * @param {Object[]} tabs 
     */
-   groupTabs(tabs) {
+   async GroupTabs() {
+      if (!this.grouping)
+      {
+         console.log("Grouping is off");
+         return;
+      }
 
+      const tabs = await browser.tabs.query({ currentWindow: true });
+
+      for (const tab of tabs) {
+
+         // Check if groups with the same domain are extendable and group if it is
+         let skip = false;
+         for (let group of this.groups) {
+            
+            if (ParseUrl(tab.url) === group.domain) {
+               // Check if the group is able to extend
+               if (!group.extendable) {
+                  skip = true;
+               }
+               // Group already existing group with the tab
+               else {
+                  // Get tabs ids in the group, then push tab id to the other ids
+                  let tabsIds = tabs.map(tab => {
+                     if (tab.groupId = group.id)
+                        return tab.id;
+                  });
+                  tabsIds.push(tab.id);
+
+                  await browser.tabs.group({ groupId: group.id, tabsIds: tabsIds });
+                  group.amount += 1;
+
+                  skip = true;
+               }
+            }
+
+         }
+         if (skip)
+            continue;
+
+         // Look for other tabs with the same domain
+         let sameDomainTabs = []; // Array of ids
+
+         for ( const tab2 of tabs ) {
+            if ( ParseUrl(tab2.url) === ParseUrl(tab.url) && tab2.pinned === false ) {
+               sameDomainTabs.push(tab2.id);
+            }
+            else {
+               continue;
+            }
+         }
+         
+         if (sameDomainTabs.length > 0) {
+            const newGroupId = await browser.tabs.group({ tabsIds: sameDomainTabs });
+            browser.tabGroups.update(newGroupId, { title: ParseUrl(tab.url) });
+         }
+
+      }
    }
 
 }
